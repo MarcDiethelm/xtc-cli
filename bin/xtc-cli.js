dcb143717cd82d559a665e1e7d79c48c6716d344
Marc Diethelm <marc@web5.me>
2014-03-31 14:38:37 +0200

    Basic cli, installing xtc works

---
+++ bin/xtc-cli.js
@@ -0,0 +1,222 @@
+#!/usr/bin/env node
+
+/**
+ * xtc-cli
+ *
+ * xtc command line tools
+ *
+ * Heavily borrowed from gulp
+ */
+
+'use strict';
+
+var c = require('chalk');
+var path = require('path');
+var inquirer = require('inquirer');
+var Liftoff = require('liftoff');
+var u = require('../lib/xtc-utils.js')
+
+var log = console.log;
+/*
+var semver = require('semver');
+var archy = require('archy');
+
+var resolve = require('resolve');
+var findup = require('findup-sync');
+var gutil = require('gulp-util');
+var prettyTime = require('pretty-hrtime');
+*/
+
+
+
+function handleArguments(env) {
+
+	var argv = env.argv;
+	var cliPackage = require('../package');
+	var versionFlag = argv.v || argv.version;
+	//var tasksFlag = argv.T || argv.tasks;
+	var command = argv._[0];
+	var args = env.argv._.slice(1);
+	//var toRun = tasks.length ? tasks : ['default'];
+
+	if (versionFlag) {
+		//log('CLI version', cliPackage.version);
+		log(cliPackage.version);
+		/*if (env.modulePackage) {
+			log('Local version', env.modulePackage.version);
+		}*/
+		process.exit(0);
+	}
+
+	/*if (process.cwd() !== env.cwd) {
+		process.chdir(env.cwd);
+		console.log('Working directory changed to', c.magenta(env.cwd));
+	}*/
+
+	else if ('help' === command || !command) {
+		log(c.magenta('\nxtc help\n'));
+	}
+
+
+	else if ('setup' === command) { // TODO: refactor this with promises and modules
+		log(c.magenta('\nxtc setup\n'));
+
+		var config = {};
+
+		inquirer.prompt([
+			{
+				type : "input",
+				name : "projectPath",
+				message : "Your project will be set up in",
+				default: env.cwd
+			}
+		], function( answers ) {
+			config.projectPath = answers.projectPath;
+
+			// get list of xtc versions from npm
+			u.pkgInfo('xtc', function(versions) {
+
+				var choices = versions.slice();
+
+				choices.unshift(new inquirer.Separator());
+				choices.push(new inquirer.Separator(), '#develop branch', new inquirer.Separator(), 'tarball', new inquirer.Separator());
+
+				// let the user choose the version to install. default to latest
+
+				inquirer.prompt([
+					{
+						type : "list",
+						name : "xtcVersion",
+						message : "Choose a  version to install",
+						paginated : true,
+						default: 0,
+						choices : choices
+					}
+				], function( answers ) {
+					var xtcInstallArg
+						,expectStr
+					;
+					config.xtcVersion = answers.xtcVersion;
+
+					if (config.xtcVersion == '#develop branch') {
+						xtcInstallArg = 'git://github.com/marcdiethelm/xtc.git#develop';
+						expectStr = 'xtc@';
+					} else if (config.xtcVersion == 'tarball') { // todo: need a prompt for tarball location
+						xtcInstallArg = '/Users/marc/projects/xtc/xtc-0.8.0-beta6.tgz';
+						expectStr = 'xtc@';
+					} else {
+						xtcInstallArg = 'xtc@'+ config.xtcVersion;
+						expectStr = 'xtc@'+ config.xtcVersion +' node_modules/xtc';
+					}
+
+					var nexpect = require('nexpect');
+
+					log(c.magenta('\nInstalling xtc module %s...\n'), config.xtcVersion);
+
+					nexpect.spawn('npm', ['install', xtcInstallArg], { verbose: true })
+						.expect(expectStr)
+						.run(function (err, stdout, exitcode) {
+							if (err) {
+								// todo
+								throw err;
+								process.exit(1);
+							}
+							log(c.cyan('\nxtc module installed successfully'));
+							//log(c.magenta('\nStarting project setup...\n'));
+
+							try {
+								require('fs').symlinkSync(path.join(process.cwd(), 'node_modules/xtc/node_modules/generator-xtc'), path.join(process.cwd(), 'node_modules/generator-xtc'), 'dir');
+							} catch (e) {
+								if (e.code === 'EEXIST') {
+									console.info(c.grey('generator-xtc already linked to node_modules'));
+								}
+								else {
+									throw e;
+								}
+							}
+
+							log(c.magenta('\nyo xtc\t\tto start the project generator.\n'));
+
+							/*nexpect.spawn('yo', ['xtc'], { verbose: true })
+								.expect('')
+								.run(function (err, stdout, exitcode) {
+									if (err) {
+										// todo
+										console.error('There was an error');
+										throw err;
+										process.exit(1);
+									}
+
+									console.log(c.cyan('\nxtc setup complete\n'));
+									process.exit(0);
+								})
+							;*/
+
+						})
+					;
+
+				});
+
+				/*var ui = new inquirer.ui.BottomBar();
+
+				// Or simply write output
+				ui.log.write("something just happened.");*/
+
+			});
+
+		});
+
+		// create a temporary package.json
+
+		// npm install xtc@version (versioned generator is in xtc's dependencies)
+
+		// run generator (generator replaces package.json)
+	}
+
+	else if ('build' === command) {
+		//u.checkLocalXtc(env);
+
+		if (!argv.dist) {
+			log(c.magenta('launching xtc dev build...'))
+		}
+		else if (argv.dist === true) {
+			log(c.magenta('launching xtc dist build...'))
+		}
+		log(c.magenta('build location: %s'), env.cwd);
+		process.exit(0);
+	}
+
+	else if ('mkmod' === command) {
+		u.checkLocalXtc(env);
+
+		var moduleNames = args;
+
+		log('creating new modules', moduleNames);
+		log(c.magenta('location: %s'), env.cwd);
+		process.exit(0);
+	}
+
+	else if ('mkskin' === command) {
+		//u.checkLocalXtc(env);
+
+		var skinNames = env.argv._.slice(1);
+
+		log('creating new modules', moduleNames);
+		log(c.magenta('location: %s'), env.cwd);
+		process.exit(0);
+	}
+
+	//log(argv);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+var cli = new Liftoff({
+	name: 'xtc'
+});
+
+//log(c.magenta('xtc cli'))
+
+cli.launch(handleArguments);
